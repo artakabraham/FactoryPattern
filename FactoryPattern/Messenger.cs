384892ff6753abe6b0f604484c1965710657510a
Artak Abrahamyan <abrahamyan@outlook.com>
2020-12-12 00:10:01 +0400

    ConnectToTwitter

---
+++ FactoryPattern/Messenger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern
+{
+    public class Messenger
+    {
+        public string UserName { get; }
+        public string Password { get; }
+        public bool Connected { get; }
+
+        public Messenger(string name, string password)
+        {
+            if (!(string.IsNullOrWhiteSpace(name) && string.IsNullOrWhiteSpace(password)))
+            {
+                UserName = name;
+                Password = password;
+                Connected = ConnectToTwitter();
+            }
+            else
+            {
+                Connected = false;
+            }
+        }
+
+        private bool ConnectToTwitter()
+        {
+            // TODO: Request to Twitter API 
+            Console.WriteLine("Twitter authentication passed");
+            return true;
+        }
+    }
+}
