dcc65e1b6d00b56bdcea25302bacb4f2a06f50b7
Artak Abrahamyan <abrahamyan@outlook.com>
2021-02-18 11:51:47 +0400

    Adding IMessage IMessenger Interfaces

---
+++ FactoryPattern/IMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern
+{
+    interface IMessage
+    {
+        public string Text { get; set; }
+        public string Target { get; set; }
+        public string Source { get; set; }
+
+        void Send();
+    }
+}
---
+++ FactoryPattern/IMessenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern
+{
+    interface IMessenger
+    {
+        string UserName { get; }
+        string Password { get; }
+        bool Connected { get; }
+        IMessage CreateMessage(string text, string target, string source);
+        bool Authorize();
+    }
+}
